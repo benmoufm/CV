6f72444a03904523e068ad447c9424c433510181
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-08-30 21:28:53 +0200

    Add navigation bar and tabBarItem for Contact view
    Change-Id: Iba65fa778f01b8e99f2925ae5bc064dda6b764da

--- CV/ContactViewController.swift
+++ CV/ContactViewController.swift
@@ -13,9 +13,12 @@ class ContactViewController: UIViewController,
     ContactViewContract {
 
     var presenter: ContactPresenter?
+    let navigationBar = UINavigationBar()
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nil, bundle: nil)
+        navigationItem.title = "contact_navigation_title_text".localized
+        tabBarItem.title = "contact_navigation_title_text".localized
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -38,7 +41,26 @@ class ContactViewController: UIViewController,
 
     // MARK: - Private methods
 
+    private func setupNavigationBar() {
+        navigationBar.setItems([navigationItem],
+                               animated: false)
+        navigationBar.barTintColor = UIColor.mainColor
+        navigationBar.alpha = 0.7
+        navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+    }
+
+    private func setupLayout() {
+        view.addSubview(navigationBar)
+
+        navigationBar.translatesAutoresizingMaskIntoConstraints = false
+        navigationBar.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        navigationBar.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        navigationBar.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        navigationBar.heightAnchor.constraint(equalToConstant: 60.0).isActive = true
+    }
+
     private func setup() {
-        // TODO: (Mélodie Benmouffek) Setup subviews
+        setupNavigationBar()
+        setupLayout()
     }
 }
