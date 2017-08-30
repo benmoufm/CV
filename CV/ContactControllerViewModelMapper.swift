4649a4e481a040b417219a0bc1f972ca9bb65b90
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-08-30 21:22:45 +0200

    Add contact view controller
    Change-Id: I2ab9f66c05d6feeabca00b905d55305f4fd44da3

---
+++ CV/ContactControllerViewModelMapper.swift
@@ -0,0 +1,17 @@
+//
+//  ContactControllerViewModelMapper.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 30/08/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+struct ContactControllerViewModelMapper {
+
+    func map() -> ContactControllerViewModel {
+        return ContactControllerViewModel()
+    }
+
+}
