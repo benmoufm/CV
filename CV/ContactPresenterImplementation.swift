ee89fe8e53a4bb99d7338cb33b76a09041774cbd
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-09-04 14:52:36 +0200

    Add action add contact
    Change-Id: I50cc1faa4959fc6675fec9a0534f3b083f4564a8

--- CV/ContactPresenterImplementation.swift
+++ CV/ContactPresenterImplementation.swift
@@ -8,15 +8,26 @@
 
 import Foundation
 import UIKit
+import Contacts
 
-class ContactPresenterImplementation: ContactPresenter {
+class ContactPresenterImplementation: UIViewController, ContactPresenter {
 
     private unowned let viewContract: ContactViewContract
+    private let contactRepository: ContactRepository
+
+    var contactInfo: CNContact
 
     // MARK: LifeCycle
 
-    init(viewContract: ContactViewContract) {
+    init(viewContract: ContactViewContract, contactRepository: ContactRepository) {
         self.viewContract = viewContract
+        self.contactRepository = contactRepository
+        contactInfo = contactRepository.melodieContact
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
     // MARK: - Startable
@@ -29,6 +40,23 @@ class ContactPresenterImplementation: ContactPresenter {
 
     func call(_ url: URL) {
         UIApplication.shared.open(url)
+    }
+
+    func createContact() {
+        contactRepository.createContact(contactInfo) { success -> Void in
+            var message: String
+            if success {
+                message = "contact_creation_success_message_popup".localized
+            } else {
+                message = "contact_creation_error_message_popup".localized
+            }
+            let alert = UIAlertController(title: "contact_creation_title_popup".localized,
+                                          message: message,
+                                          preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "contact_creation_button_text_popup".localized,
+                                          style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 
     // MARK: - Private methods
