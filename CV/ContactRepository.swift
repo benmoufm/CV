d579db6cb97e4d52f6465edd9df2e7f58f70cf22
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-09-04 14:40:32 +0200

    Create RepositoryFactory and ContactRepository
    Change-Id: I6b9ef5a19b3cdfe30aa905515d5b5723d4358997

---
+++ CV/ContactRepository.swift
@@ -0,0 +1,15 @@
+//
+//  ContactRepository.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 03/09/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+import Contacts
+
+protocol ContactRepository {
+    var melodieContact: CNContact { get }
+    func createContact(_ contact: CNContact, _ completion: ((Bool) -> Void)?)
+}
