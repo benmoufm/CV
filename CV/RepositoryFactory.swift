bd6ca31066cd481bcf9e6f97bade57617759614f
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-09-04 14:40:32 +0200

    Create RepositoryFactory and ContactRepository
    Change-Id: I6b9ef5a19b3cdfe30aa905515d5b5723d4358997

---
+++ CV/RepositoryFactory.swift
@@ -0,0 +1,18 @@
+//
+//  RepositoryFactory.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 03/09/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+final class RepositoryFactory {
+
+    static let sharedInstance = RepositoryFactory()
+    private(set) lazy var contactRepository: ContactRepository = LocalContactRepository()
+
+    private init() {}
+
+}
