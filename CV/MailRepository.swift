d99eb4ed9ed242500782d88cbd02e52ea804d7b0
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-09-04 15:20:56 +0200

    Create Mail repository
    Change-Id: I86ed6177c11ed955f4444da714c3db73bb296423

---
+++ CV/MailRepository.swift
@@ -0,0 +1,14 @@
+//
+//  MailRepository.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 04/09/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+import Contacts
+
+protocol MailRepository {
+    func sendMail(_ contact: CNContact, _ completion: ((String) -> Void)?)
+}
