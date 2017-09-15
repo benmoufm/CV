7c888b324954abe609afb3e5788070d433e1580f
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-09-15 17:31:19 +0200

    CleanUp selection/deselection and add multiple contacts
    Change-Id: I45cc31d24c003b6547d0617d11094c324a3f9d4c

--- CV/ContactTableViewCell.swift
+++ CV/ContactTableViewCell.swift
@@ -11,10 +11,43 @@ import UIKit
 
 class ContactTableViewCell: UITableViewCell {
 
+    let checkImageView = UIImageView(image: #imageLiteral(resourceName: "checkIcon"))
+
+    // MARK: - Lifecycle
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupLayout()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupLayout()
+    }
+
+    // MARK: - ContactTableViewCell
 
     func configure(with viewModel: ContactCellViewModel) {
         imageView?.image = viewModel.image
         textLabel?.text = viewModel.label
         textLabel?.textColor = viewModel.textColor
+        switch viewModel.cellType {
+        case .contact:
+            checkImageView.isHidden = !viewModel.isSelected
+        default:
+            break
+        }
+    }
+
+    // MARK: - Private methods
+
+    private func setupLayout() {
+        checkImageView.isHidden = true
+        self.addSubview(checkImageView)
+        checkImageView.translatesAutoresizingMaskIntoConstraints = false
+        checkImageView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.5).isActive = true
+        checkImageView.widthAnchor.constraint(equalTo: heightAnchor, multiplier: 0.5).isActive = true
+        checkImageView.rightAnchor.constraint(equalTo: rightAnchor, constant: -20.0).isActive = true
+        checkImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
     }
 }
