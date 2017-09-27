f2f47e4c672f2612e9af49f60028f4a725a71b1d
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-09-27 12:55:59 +0200

    Add presentation view controller
    Change-Id: I57945f68694d43e3fbb96f64f9fa695057f7e4ab

---
+++ CV/PresentationControllerViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PresentationControllerViewModel.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 27/09/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+struct PresentationControllerViewModel {
+
+}
