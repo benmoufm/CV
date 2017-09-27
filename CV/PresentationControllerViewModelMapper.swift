7fdd286d4661b731b4715bd31290ec5db305e671
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-09-27 12:55:59 +0200

    Add presentation view controller
    Change-Id: I57945f68694d43e3fbb96f64f9fa695057f7e4ab

---
+++ CV/PresentationControllerViewModelMapper.swift
@@ -0,0 +1,17 @@
+//
+//  PresentationControllerViewModelMapper.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 27/09/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+struct PresentationControllerViewModelMapper {
+
+    func map() -> PresentationControllerViewModel {
+        return PresentationControllerViewModel()
+    }
+
+}
---
+++ CV/PresentationViewController.swift
@@ -0,0 +1,45 @@
+//
+//  PresentationViewController.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 27/09/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PresentationViewController: UIViewController,
+    PresentationViewContract {
+
+    var presenter: PresentationPresenter?
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - UIViewController
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        presenter?.start()
+    }
+
+    // MARK: - PresentationViewContract
+
+    func configure(with viewModel: PresentationControllerViewModel) {
+        // TODO: (Mélodie Benmouffek) 27/09/2017 Configure view
+    }
+
+    // MARK: - Private methods
+
+    private func setup() {
+        // TODO: (Mélodie Benmouffek) 27/09/2017 Setup subviews
+    }
+}
