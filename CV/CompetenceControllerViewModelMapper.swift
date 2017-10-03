67a36083a0dbf9ebc8dc89e67bb32d973340fc43
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-10-03 17:23:35 +0200

    Add competence view controller
    Change-Id: I37571b6f788806dc50078cf75385c9e3648ef7e8

---
+++ CV/CompetenceControllerViewModelMapper.swift
@@ -0,0 +1,17 @@
+//
+//  CompetenceControllerViewModelMapper.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 03/10/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+struct CompetenceControllerViewModelMapper {
+
+    func map() -> CompetenceControllerViewModel {
+        return CompetenceControllerViewModel()
+    }
+
+}
---
+++ CV/CompetenceViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CompetenceViewController.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 03/10/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CompetenceViewController: UIViewController,
+    CompetenceViewContract {
+
+    var presenter: CompetencePresenter?
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - UIViewController
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        presenter?.start()
+    }
+
+    // MARK: - CompetenceViewContract
+
+    func configure(with viewModel: CompetenceControllerViewModel) {
+        // TODO: (Mélodie Benmouffek) 03/10/2017 Configure view
+    }
+
+    // MARK: - Private methods
+
+    private func setup() {
+        // TODO: (Mélodie Benmouffek) 03/10/2017 Setup subviews
+    }
+}
