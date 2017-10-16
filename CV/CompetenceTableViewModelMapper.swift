a64bc1f1fa2216550e5d7e77aa432d9570123b1d
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-10-16 22:25:38 +0200

    Populate the table view for Competence tab
    Change-Id: I59db612d71fd690f4dc84400c19a16433a747df8

--- CV/CompetenceTableViewModelMapper.swift
+++ CV/CompetenceTableViewModelMapper.swift
@@ -11,6 +11,34 @@ import Foundation
 struct CompetenceTableViewModelMapper {
 
     func map() -> CompetenceTableViewModel {
-        return CompetenceTableViewModel(tableCells: [])
+        // Titles
+        let developmentCell = CompetenceCellViewModelMapper(label: "development_cells".localized, isTitleCell: true).map()
+        let developmentToolsCell = CompetenceCellViewModelMapper(label: "development_tools_cells".localized, isTitleCell: true).map()
+        let patternsCell = CompetenceCellViewModelMapper(label: "pattern_cells".localized, isTitleCell: true).map()
+        let methodsCell = CompetenceCellViewModelMapper(label: "methods_cells".localized, isTitleCell: true).map()
+        let languageCell = CompetenceCellViewModelMapper(label: "languages_cells".localized, isTitleCell: true).map()
+
+        // Cells
+        let swiftCell = CompetenceCellViewModelMapper(label: "swift_cell".localized, isTitleCell: false).map()
+        let javaCell = CompetenceCellViewModelMapper(label: "java_cell".localized, isTitleCell: false).map()
+        let cSharpCell = CompetenceCellViewModelMapper(label: "c_sharp_cell".localized, isTitleCell: false).map()
+        let htmlCell = CompetenceCellViewModelMapper(label: "html_cell".localized, isTitleCell: false).map()
+        let xcodeCell = CompetenceCellViewModelMapper(label: "xcode_cell".localized, isTitleCell: false).map()
+        let androidStudioCell = CompetenceCellViewModelMapper(label: "android_studio_cell".localized, isTitleCell: false).map()
+        let mvcCell = CompetenceCellViewModelMapper(label: "mvc_cell".localized, isTitleCell: false).map()
+        let cleanCodeCell = CompetenceCellViewModelMapper(label: "clean_code_cell".localized, isTitleCell: false).map()
+        let agileCell = CompetenceCellViewModelMapper(label: "agile_cell".localized, isTitleCell: false).map()
+        let gitCell = CompetenceCellViewModelMapper(label: "git_cell".localized, isTitleCell: false).map()
+        let gerritCell = CompetenceCellViewModelMapper(label: "gerrit_cell".localized, isTitleCell: false).map()
+        let englishCell = CompetenceCellViewModelMapper(label: "english_cell".localized, isTitleCell: false).map()
+        let spanishCell = CompetenceCellViewModelMapper(label: "spanish_cell".localized, isTitleCell: false).map()
+        let italianCell = CompetenceCellViewModelMapper(label: "italian_cell".localized, isTitleCell: false).map()
+        let koreanCell = CompetenceCellViewModelMapper(label: "korean_cell".localized, isTitleCell: false).map()
+
+        return CompetenceTableViewModel(tableCells: [developmentCell, swiftCell, javaCell, cSharpCell, htmlCell,
+                                                     developmentToolsCell, xcodeCell, androidStudioCell,
+                                                     patternsCell, mvcCell, cleanCodeCell,
+                                                     methodsCell, agileCell, gitCell, gerritCell,
+                                                     languageCell, englishCell, spanishCell, italianCell, koreanCell])
     }
 }
