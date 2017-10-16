e3b8273009a8b959b6b7c47dd384a44dcf1717fc
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-10-16 14:07:00 +0200

    Add experience view controller
    Change-Id: I146999b18ca8b1d67efefb59e9965f1eb3df7ca2

---
+++ CV/ExperienceViewContract.swift
@@ -0,0 +1,14 @@
+//
+//  ExperienceViewContract.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 16/10/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+protocol ExperienceViewContract: class {
+
+    func configure(with viewModel: ExperienceControllerViewModel)
+}
