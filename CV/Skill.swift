76ef0ded8290095dfa9017722bc9e2024a01587b
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-10-23 23:29:35 +0200

    Add skill entities for Competence tab
    Change-Id: Ie50c1869152e88d35e5ba9fe585d61993a6f16f7

---
+++ CV/Skill.swift
@@ -0,0 +1,16 @@
+//
+//  Skill.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 23/10/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+struct Skill {
+    let id: Int
+    let name: String
+    let evaluation: Int
+    let usageDescription: String
+}
---
+++ CV/SkillCategory.swift
@@ -0,0 +1,16 @@
+//
+//  SkillCategory.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 23/10/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct SkillCategory {
+    let id: Int
+    let name: String
+    let skills: [Skill]
+}
