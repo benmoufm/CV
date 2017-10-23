510ff26b836438096db598a0dfe43a3f49371889
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-10-23 15:12:28 +0200

    Add TableView, ViewModel, Mapper and DataSource for Experience tab
    Change-Id: I587e4647caf72203deb47b64d63edf6cd308022f

---
+++ CV/ExperienceTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  ExperienceTableViewCell.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 23/10/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ExperienceTableViewCell: UITableViewCell {
+
+    // MARK: - Lifecycle
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    // MARK: - ExperienceTableViewCell
+
+    func configure(with viewModel: ExperienceCellViewModel) {
+
+    }
+}
