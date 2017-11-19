2df588e8b8cb04af2752fc30277b5badec45f7de
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-11-19 19:54:51 +0100

    Add Http Manager
    Change-Id: Ibb6fe78d0c9108059f18f21fa898f4d0fcfcc476

---
+++ CV/HttpManager.swift
@@ -0,0 +1,44 @@
+//
+//  HttpManager.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 19/11/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class HttpManager {
+
+    let sessionManager: SessionManager
+
+    init(sessionManager: SessionManager) {
+        self.sessionManager = sessionManager
+    }
+
+    func execute(httpRequest: HttpRequest, _ completion: ((Result<JSON>) -> Void)?) {
+        guard let url = URL(string: "https://demo6086858.mockable.io/\(httpRequest.domain)")
+            else {
+                completion?(.error(CVError.urlFormatError as NSError))
+                return
+        }
+        sessionManager.request(url,
+                               method: httpRequest.method,
+                               parameters: httpRequest.parameters,
+                               encoding: httpRequest.encoding,
+                               headers: httpRequest.headers)
+            .responseJSON { (responseData) -> Void in
+                let result: Result<JSON>
+                switch responseData.result {
+                case .success(let data):
+                    result = .value(JSON(data))
+                case .failure(let error):
+                    result = .error(error as NSError)
+                }
+                completion?(result)
+        }
+    }
+
+}
