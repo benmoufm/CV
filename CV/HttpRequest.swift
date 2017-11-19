23302e30cad28f2576f9c2b479a0339b0704e254
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-11-19 22:44:26 +0100

    Add HttpRequest
    Change-Id: Ia2d906bc1f54b1933db07cfef1b0f35aa5215455

---
+++ CV/HttpRequest.swift
@@ -0,0 +1,33 @@
+//
+//  HttpRequest.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 19/11/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+struct HttpRequest {
+
+    init(url: URL,
+         method: HTTPMethod = HTTPMethod.get,
+         headers: HTTPHeaders = HTTPHeaders.init(),
+         encoding: URLEncoding = URLEncoding.default,
+         parameters: [String: Any] = [String: Any]())
+    {
+        self.url = url
+        self.method = method
+        self.headers = headers
+        self.encoding = encoding
+        self.parameters = parameters
+    }
+
+    let url: URL
+    let method: HTTPMethod
+    let headers: HTTPHeaders
+    let encoding: URLEncoding
+    let parameters: [String: Any]
+
+}
