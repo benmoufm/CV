4566e2966cbe59d1c6149508692d06d23de8aec7
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-11-22 15:19:30 +0100

    Add skill parsing repository
    Change-Id: I5997c56137840162288234e0ae6601151171d2bb

---
+++ CV/SkillsRepositoryImplementation.swift
@@ -0,0 +1,53 @@
+//
+//  SkillsRepositoryImplementation.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 21/11/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+import Alamofire
+
+class SkillsRepositoryImplementation: SkillsRepository {
+
+    let httpManager: HttpManager
+
+    init() {
+        let configuration = URLSessionConfiguration.default
+        let sessionManager = Alamofire.SessionManager(configuration: configuration)
+        httpManager = HttpManager(sessionManager: sessionManager)
+    }
+
+    func getSkillCategories(completion: ((Result<[SkillCategory]>) -> Void)?) {
+        var resultSkillCategories = Result<[SkillCategory]>([])
+        guard let url = URL(string: "https://demo6086858.mockable.io/skills") else {
+            resultSkillCategories = .error(CVError.urlFormatError as NSError)
+            completion?(resultSkillCategories)
+            return
+        }
+        let httpRequest = HttpRequest(url: url)
+        httpManager.execute(httpRequest: httpRequest) { result -> Void in
+            switch result {
+            case .error(let error):
+                resultSkillCategories = .error(error as NSError)
+            case .value(let json):
+                do {
+                    if let jsonCategories = json["categories"].array {
+                        let categories = try jsonCategories
+                            .map { try RestSkillCategory(json: $0) }
+                            .map { SkillCategoryMapper(restSkillCategory: $0).map() }
+                        resultSkillCategories = .value(categories)
+                    } else {
+                        resultSkillCategories = .error(CVError.unexpectedJSONFormat as NSError)
+                    }
+                } catch {
+                    resultSkillCategories = .error(error as NSError)
+                }
+            }
+            completion?(resultSkillCategories)
+        }
+    }
+
+}
