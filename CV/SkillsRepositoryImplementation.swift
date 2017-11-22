708568adf3e34b7b14b1d399372479c122c25d3d
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-11-22 15:19:30 +0100

    Add skill parsing repository
    Change-Id: I5997c56137840162288234e0ae6601151171d2bb

---
+++ CV/SkillsRepositoryImplementation.swift
@@ -0,0 +1,38 @@
+//
+//  SkillsRepositoryImplementation.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 21/11/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+import Alamofire
+
+class SkillsRepositoryImplementation: SkillsRepository {
+
+    let httpManager: HttpManager
+
+    init() {
+        let configuration = URLSessionConfiguration.default
+        let sessionManager = Alamofire.SessionManager(configuration: configuration)
+        httpManager = HttpManager(sessionManager: sessionManager)
+    }
+
+    func getSkillCategories(completion: ((Result<[SkillCategory]>) -> Void)?) {
+        let httpRequest = HttpRequest(endPoint: "skills")
+        httpManager.execute(httpRequest: httpRequest) { result -> Void in
+            let resultSkillCategories = result.map({ (json) -> [SkillCategory] in
+                guard let jsonCategories = json["categories"].array
+                    else { throw CVError.unexpectedJSONFormat }
+                let categories = try jsonCategories
+                    .map { try RestSkillCategory(json: $0) }
+                    .map { SkillCategoryMapper(restSkillCategory: $0).map() }
+                return categories
+            })
+            completion?(resultSkillCategories)
+        }
+    }
+
+}
