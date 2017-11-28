41a81b8288735fed80664725712273e4a062f517
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-11-28 15:18:04 +0100

    Add rest object skill
    Change-Id: I91262566d1325bc5dd125dec92a4eec29ddb8bc6

---
+++ CV/RestSkill.swift
@@ -0,0 +1,38 @@
+//
+//  RestSkill.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 28/11/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct RestSkill {
+    let id: Int
+    let name: String
+    let category: SkillCategory
+    let evaluation: Int
+    let usageDescription: String
+
+    init(json: JSON) throws {
+        guard
+            let id = json["id"].int,
+            let name = json["name"].string,
+            let categoryID = json["skill_category_id"].int,
+            let category = SkillCategory(rawValue: categoryID),
+            let evaluation = json["value"].int,
+            let usageDescription = json["description"].string
+        else
+        {
+            throw CVError.unexpectedJSONFormat
+        }
+
+        self.id = id
+        self.name = name
+        self.category = category
+        self.evaluation = evaluation
+        self.usageDescription = usageDescription
+    }
+}
