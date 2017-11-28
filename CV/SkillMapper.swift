3d6d372b40c01e56aa031765d2a4c484d95e5f10
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-11-28 15:53:26 +0100

    Add Skill mapper
    Change-Id: Ib0ec77040de7d48a21aeefcc1d7eb9986ae69cba

---
+++ CV/SkillMapper.swift
@@ -0,0 +1,23 @@
+//
+//  SkillMapper.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 28/11/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+struct SkillMapper {
+    let restSkill: RestSkill
+
+    func map() -> Skill {
+        return Skill (
+            name: restSkill.name,
+            isCategory: restSkill.isCategory,
+            category: restSkill.category,
+            evaluation: restSkill.evaluation,
+            usageDescription: restSkill.usageDescription
+        )
+    }
+}
