f8beb59ecec2f9e043e3d01e7c722149769c500e
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-12-04 12:02:46 +0100

    Set up competence detail view controller layout
    Change-Id: I4cb35d68f6f3baa5ff8c8688849542e6b7a07778

---
+++ CV/CompetenceDetailViewContract.swift
@@ -0,0 +1,14 @@
+//
+//  CompetenceDetailViewContract.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 04/12/2017.
+//  Copyright © 2017 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+protocol CompetenceDetailViewContract: class {
+
+    func configure(with viewModel: CompetenceDetailControllerViewModel)
+}
