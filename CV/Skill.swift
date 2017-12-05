2666ceea5ed357aaea50cd2ae37cfc22ff681740
Melodie Benmouffek <m.benmouffek@gmail.com>
2017-12-05 16:13:44 +0100

    Add fetch skill by id function to repository
    Change-Id: I5b1af126541ccd8bc968049f3dbd60e0c7e799cd

--- CV/Skill.swift
+++ CV/Skill.swift
@@ -9,6 +9,21 @@
 import Foundation
 
 struct Skill {
+
+    init() {
+        self.id = -1
+        self.name = ""
+        self.evaluation = 0
+        self.usageDescription = ""
+    }
+
+    init(id: Int, name: String, evaluation: Int, usageDescription: String) {
+        self.id = id
+        self.name = name
+        self.evaluation = evaluation
+        self.usageDescription = usageDescription
+    }
+
     let id: Int
     let name: String
     let evaluation: Int
--- CV/SkillsRepositoryImplementation.swift
+++ CV/SkillsRepositoryImplementation.swift
@@ -50,4 +50,38 @@ class SkillsRepositoryImplementation: SkillsRepository {
         }
     }
 
+    func getSkillById(_ id: Int, completion: ((Result<Skill>) -> Void)?) {
+        var resultSkill = Result<Skill>(Skill())
+        guard let url = URL(string: "https://demo6086858.mockable.io/skills") else {
+            resultSkill = .error(CVError.urlFormatError as NSError)
+            completion?(resultSkill)
+            return
+        }
+        let httpRequest = HttpRequest(url: url)
+        httpManager.execute(httpRequest: httpRequest) { result in
+            switch result {
+            case .error(let error):
+                resultSkill = .error(error as NSError)
+            case .value(let json):
+                do {
+                    if let jsonCategories = json["categories"].array {
+                        let skill = try jsonCategories
+                            .map { try RestSkillCategory(json: $0) }
+                            .map { SkillCategoryMapper(restSkillCategory: $0).map() }
+                            .map { $0.skills.filter { $0.id == id } }
+                            .flatMap{ $0 }
+                        if let uniqueSkill = skill.first {
+                            resultSkill = .value(uniqueSkill)
+                        } else {
+                            resultSkill = .error(CVError.unknownSkillId as NSError)
+                        }
+                    }
+                } catch {
+                    resultSkill = .error(error as NSError)
+                }
+            }
+            completion?(resultSkill)
+        }
+    }
+
 }
