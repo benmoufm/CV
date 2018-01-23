fb666aa987585828730d00e9baf2b377050dade3
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-23 19:52:42 +0100

    Create rest experience category object and mapper
    Change-Id: I8aecd4802caecdc1d1060cf46784300fffe5bbc2

---
+++ CV/ExperienceCategoryMapper.swift
@@ -0,0 +1,19 @@
+//
+//  RestExperienceCategoryMapper.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 23/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+struct ExperienceCategoryMapper {
+    let restExperienceCategory: RestExperienceCategory
+
+    func map() -> ExperienceCategory {
+        return ExperienceCategory(id: restExperienceCategory.id,
+                                  name: restExperienceCategory.name,
+                                  experiences: restExperienceCategory.experiences.map { ExperienceMapper(restExperience: $0).map() })
+    }
+}
