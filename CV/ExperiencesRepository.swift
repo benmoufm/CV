762d382d37bfa22710996dc8fc9158ccd5c1aaa1
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-23 18:33:21 +0100

    Create experiences repository protocol
    Change-Id: If10e121e230824ebe0e3e78e11c833bdb1229bae

---
+++ CV/ExperiencesRepository.swift
@@ -0,0 +1,14 @@
+//
+//  ExperiencesRepository.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 23/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+protocol ExperiencesRepository {
+    func getExperienceCategories(completion: ((Result<[ExperienceCategory]>) -> Void)?)
+    func getExperienceById(_ id: Int, completion: ((Result<Experience>) -> Void)?)
+}
