8006ff8c4e2cfedfea9945ad41216bf21b08e4dd
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-23 18:26:29 +0100

    Create experience category entity
    Change-Id: I8add1a43298ded19f6792f5ae1cc450b92fab794

---
+++ CV/ExperienceCategory.swift
@@ -0,0 +1,15 @@
+//
+//  ExperienceCategory.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 23/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+struct ExperienceCategory {
+    let id: Int
+    let name: String
+    let experiences: [Experience]
+}
