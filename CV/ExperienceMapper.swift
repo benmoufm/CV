7e52292779725195f003837eb33c7669139b3650
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-23 19:42:02 +0100

    Create rest experience object and mapper
    Change-Id: I08f0f60693e1971852add4ccb7352bf9a5f79b24

---
+++ CV/ExperienceMapper.swift
@@ -0,0 +1,21 @@
+//
+//  RestExperienceMapper.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 23/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+struct ExperienceMapper {
+    let restExperience: RestExperience
+
+    func map() -> Experience {
+        return Experience(id: restExperience.id,
+                          name: restExperience.name,
+                          timeStamp: restExperience.timeStamp,
+                          skillsID: restExperience.skillsID,
+                          description: restExperience.description)
+    }
+}
