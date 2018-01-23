01b7de8f1b1a32f2705235de0072265d6d215891
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-23 19:52:42 +0100

    Create rest experience category object and mapper
    Change-Id: I8aecd4802caecdc1d1060cf46784300fffe5bbc2

---
+++ CV/RestExperienceCategory.swift
@@ -0,0 +1,29 @@
+//
+//  RestExperienceCategory.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 23/01/2018.
+//  Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct RestExperienceCategory {
+    let id: Int
+    let name: String
+    let experiences: [RestExperience]
+
+    init(json: JSON) throws {
+        guard
+            let id = json["id"].int,
+            let name = json["name"].string,
+            let jsonExperiences = json["experiences"].array
+            else { throw CVError.unexpectedJSONFormat }
+        let restExperiences = try jsonExperiences.map { try RestExperience(json: $0) }
+
+        self.id = id
+        self.name = name
+        self.experiences = restExperiences
+    }
+}
