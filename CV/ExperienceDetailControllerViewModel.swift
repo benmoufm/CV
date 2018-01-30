2023f54a575ce1fd66b7c4ff8c272a363d462f5e
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-30 16:49:53 +0100

    Create experience detail view controller
    Change-Id: Ibd74298dcfc8932775a66a3cf58cc145630b2179

---
+++ CV/ExperienceDetailControllerViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ExperienceDetailControllerViewModel.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 30/01/2018.
+//Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+struct ExperienceDetailControllerViewModel {
+
+}
---
+++ CV/ExperienceDetailViewContract.swift
@@ -0,0 +1,14 @@
+//
+//  ExperienceDetailViewContract.swift
+//  CV
+//
+//  Created by Mélodie Benmouffek on 30/01/2018.
+//Copyright © 2018 Mélodie Benmouffek. All rights reserved.
+//
+
+import Foundation
+
+protocol ExperienceDetailViewContract: class {
+
+    func configure(with viewModel: ExperienceDetailControllerViewModel)
+}
