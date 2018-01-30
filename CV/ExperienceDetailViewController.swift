5aaaf9d6b866b455f1232108dab94ec1cd573bc7
Melodie Benmouffek <m.benmouffek@gmail.com>
2018-01-30 17:56:36 +0100

    Setup experience detail view controller layout
    Change-Id: I6addf4aea1aba3e0c9bbb9ac8f8dceee6f2929ad

--- CV/ExperienceDetailViewController.swift
+++ CV/ExperienceDetailViewController.swift
@@ -13,6 +13,9 @@ class ExperienceDetailViewController: UIViewController,
     ExperienceDetailViewContract {
 
     var presenter: ExperienceDetailPresenter?
+    let titleLabel = UILabel()
+    let dateLabel = UILabel()
+    let descriptionLabel = UILabel()
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nil, bundle: nil)
@@ -47,8 +50,54 @@ class ExperienceDetailViewController: UIViewController,
             [NSForegroundColorAttributeName: UIColor.navigationTitleTextColor]
     }
 
+    private func setupTitleLabel() {
+        titleLabel.numberOfLines = 0
+        titleLabel.textAlignment = .center
+        titleLabel.textColor = UIColor.mainColorWithAlpha
+        titleLabel.font = UIFont.titleFont
+    }
+
+    private func setupDateLabel() {
+        dateLabel.numberOfLines = 0
+        dateLabel.textAlignment = .center
+        dateLabel.textColor = UIColor.textColor
+        dateLabel.font = UIFont.captionFont
+    }
+
+    private func setupDescriptionLabel() {
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.textColor = UIColor.textColor
+    }
+
+    private func setupLayout() {
+        view.addSubview(titleLabel)
+        view.addSubview(dateLabel)
+        view.addSubview(descriptionLabel)
+
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        titleLabel.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 50.0).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100.0).isActive = true
+
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        dateLabel.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        dateLabel.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        dateLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor).isActive = true
+        dateLabel.heightAnchor.constraint(equalToConstant: 20.0).isActive = true
+
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40.0).isActive = true
+        descriptionLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40.0).isActive = true
+        descriptionLabel.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 50.0).isActive = true
+    }
+
     private func setup() {
         view.backgroundColor = UIColor.backgroundColor
         setupNavigationBar()
+        setupTitleLabel()
+        setupDateLabel()
+        setupDescriptionLabel()
+        setupLayout()
     }
 }
